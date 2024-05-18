1096bf976bc085dbd9f4937cf1cec0a7a4c3c9c4
Felipe <felipenafe@gmail.com>
2024-05-18 16:18:21 -0300

    Felipe Fernandes rm:554589

--- src/js/script.js
+++ src/js/script.js
@@ -40,3 +40,36 @@ function validar(){
         alert("usuario e senha inválidos");
     }
 }
+
+// VALIDANDO OS CAMPOS DO CONTATO
+
+// Declarando as variáveis
+const form = document.querySelector("#form")
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const telInput = document.querySelector("#telefone")
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    //verifica se o nome esta vazio
+    if(nameInput.value === ""){
+        alert("Preencha seu nome")
+        return;
+    }
+
+    //verifica se o email esta vazio
+    if(emailInput.value === ""){
+        alert("Preencha seu seu email")
+        return;
+    }
+
+    //verifica se o telefone esta vazio
+    if(telInput.value === ""){
+        alert("Preencha seu seu telefone")
+        return;
+    }
+
+  // se todos os capos estiverem preenchidos enviar form
+  form.submit();
+});
